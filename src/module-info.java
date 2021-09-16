58701bd47a4cbe784ec11e0a39d72305da9fdcbb
Rohit Nagar <rohitnagar1993@gmail.com>
2021-09-16 18:47:16 +0530

    First Commit

---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module Framework {
+	requires org.testng;
+	requires junit;
+}
---
+++ src/test/Day1.java
@@ -0,0 +1,14 @@
+ package test;
+ import org.testng.annotations.Test;
+
+public class Day1 {
+	
+	
+  @Test
+public void Demo()
+{
+		System.out.println("hello");
+
+	}
+
+}
